0cb032b340702ece425c731a3b8a7a45455c16c3
AcoderRose <addyro96@att.net>
2024-08-13 20:06:23 -0700

    Added initial webpack configuration for development and PWA setup.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
+    // starting points for the app
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
+
+    // settings for the output
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
+
+    // Plugins used in the build process
     plugins: [
-      
+      // Generates an HTML file based on the specified template and title
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE",
+      }),
+
+      // Creates the Webpack PWA manifest for the text editor application
+      new WebpackPwaManifest({
+        fingerprints: false, // Ensures the icon displays correctly in the application
+        name: "JATE Text Editor",
+        short_name: "JATE",
+        description: "A text editor that can be accessed through a browser",
+        background_color: "#36454F",
+        theme_color: "#36454F",
+        start_url: "./",
+        publicPath: "./",
+        display: "standalone",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+
+      // Injects a service worker into the build using Workbox
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
     ],
 
+    // Rules for processing various file types
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
